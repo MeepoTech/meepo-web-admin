0a0bf7185c2735eb499d6c5bc6c6a682703a3afb
Ace <madaokuan@gmail.com>
2013-09-18 15:16:30 +0800

    rewrite upload logic

--- index.py
+++ index.py
@@ -2,25 +2,30 @@
 
 import web
 import json
-import os
 import redis
 import time
-from web.contrib.template import render_jinja
 
-upload_dir = '/home/ace/'
-render = render_jinja("templates", encoding="utf-8")
+upload_dir = '/var/www/campus-website/upload'
+render = web.template.render("templates")
 platform_list = ['win32', 'win64', 'linux32', 'linux64', 'osx', 'android']
 
 urls = (
-    #'/',            'Index',
+    '/',            'Login',
     '/upload',      'Upload',
+    '/home',        'Home',
 )
 
 r = redis.StrictRedis(host="0.meepotech.com")
 
 
-def notfound():
-    return web.notfound(render.notfound())
+class Login:
+    def GET(self):
+        return render.login()
+
+
+class Home:
+    def GET(self):
+        return render.index()
 
 
 class Upload:
@@ -37,16 +42,26 @@ class Upload:
 
     def POST(self):
         form = web.input(myfile={})
+
+        # ckeck platform
         platform = form['platform']
-        version = form['version']
-        filename = form['myfile'].filename
         if platform not in platform_list:
             return 'invalid platform type'
+
+        # read metadata
+        version = form['version']
+        filename = form['myfile'].filename
         date = time.strftime('%Y-%m-%d', time.localtime(time.time()))
-        hash_key = 'release:' + platform
-        r.hset(hash_key, 'version', version)
-        r.hset(hash_key, 'date', date)
-        r.hset(hash_key, 'filename', filename)
+        redis_key = 'release:' + platform
+        meta = {
+            'version':   version,
+            'filename':  filename,
+            'date':      date
+        }
+
+        r.set(redis_key, json.dumps(meta))
+
+        # save the file
         output = open(upload_dir + filename, 'w')
         output.write(form['myfile'].value)
 
@@ -54,7 +69,6 @@ class Upload:
 
 
 app = web.application(urls, globals())
-app.notfound = notfound
 application = app.wsgifunc()
 
 if __name__ == "__main__":
