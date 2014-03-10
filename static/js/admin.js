2c79f512e40649e279cafab8a46ef005942432f6
Zhiyuan Ai <aizhiyuan@meepotech.com>
2014-03-10 21:08:15 +0800

    statistic

--- static/js/admin.js
+++ static/js/admin.js
@@ -2388,7 +2388,7 @@ function parseSize(size,quota){
 	return {
 		used : usage,
 		quota: quota + " GB",
-		percent: Number(size / totalVolume).toFixed(2) + "%"
+		percent: Number(size / totalVolume * 100).toFixed(2) + "%"
 	};
 }
 
